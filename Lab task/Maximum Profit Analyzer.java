6c0ea273f96b8e39901b63cbcb8fcf79d2e53bc9
srirenu246-spec <srirenu246@gmail.com>
2026-08-28 10:05:44 +0530

    Add files via upload

---
+++ Lab task/Maximum Profit Analyzer.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class MaximumProfitAnalyzer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        System.out.println("Maximum Profit = " + maxProfit);
+
+        sc.close();
+    }
+}
